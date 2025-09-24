ba680b52119a8ba8e495cc7b3606a6847c260808
Jesus Aliso <alisoquintero@gmail.com>
2025-09-24 16:17:59 +0200

    test1

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).send('ok'); // Responde con "ok" como espera el workflow
+});
+
 app.get('/', (req, res) => {
   res.status(200).json({
     message: '¡Hola mundo!',
